9a5c9a7f02db6dc7065e6fda018037485d94270a
Novak Maksym <maksym.novak08@gmail.com>
2024-11-27 17:21:24 +0200

    Relocate code from Caliot_Camera_temp to camera.h

--- camera.h
+++ camera.h
@@ -27,5 +27,15 @@ void camera_config(){
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
 }
-
+  config.pixel_format = PIXFORMAT_JPEG; 
+ 
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA; 
+    config.jpeg_quality = 10; 
+    config.fb_count = 2;   
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12; 
+    config.fb_count = 1;
+  }
 #endif
